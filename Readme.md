11f2f53e1b9f97b83504751ce63993ef2c3872e9
shubhankar90 <shubhankar90@gmail.com>
2016-09-18 20:00:32 -0300

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Parametric-Modelling
+User defined parametric model fitting to data.
+
+The aim of this project is to enclose scipy least_squares function in a sklearn type interface for easier usage.
+
+The class is useable as given in the Parametric Modelling jupyter notebook with only the basic functionality of sklearn.
+
+Currently a work in progress.
---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 18 13:21:02 2016
+
+@author: Shubhankar Mitra
+"""
+
+from setuptools import setup
+
+setup(name='remoutliers',
+      version='0.4',
+      description='An Sklearn interface around scipy.optimize.least_squares for parametric modeling',
+      url='http://github.com/shubhankar90/ParametricModeling',
+      author='Shubhankar Mitra',
+      author_email='shubhankar90@gmail.com',
+      license='GNU GENERAL PUBLIC LICENSE',
+      packages=['ParametricModeling'],
+      install_requires=[
+          'scipy','numpy','sklearn'
+      ],
+      zip_safe=False)
