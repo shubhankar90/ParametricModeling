6b3115afc74a279273e15ebb5158441fbaa07135
shubhankar90 <shubhankar90@gmail.com>
2016-09-21 13:25:10 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ Created on Sat Jun 18 13:21:02 2016
 
 from setuptools import setup
 
-setup(name='remoutliers',
-      version='0.4',
+setup(name='ParametricModeling',
+      version='0.1.9',
       description='An Sklearn interface around scipy.optimize.least_squares for parametric modeling',
       url='http://github.com/shubhankar90/ParametricModeling',
       author='Shubhankar Mitra',
       author_email='shubhankar90@gmail.com',
-      license='GNU GENERAL PUBLIC LICENSE',
+      license='BSD 3 clause',
       packages=['ParametricModeling'],
       install_requires=[
           'scipy','numpy','sklearn'
